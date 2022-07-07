236b027e79d09d3ea150690e68ce730499e6a3a4
Nirbhay Adhikari <fearalert1@gmail.com>
2022-07-07 09:18:11 +0545

    serializers start vayo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,8 +124,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL= '/images/'
+
 
 CORS_ALLOW_ALL_ORIGINS= True
+
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT= 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
