5a2f8b2b2a86ef1089aa631956b8940b74312c6b
Nirbhay Adhikari <fearalert1@gmail.com>
2022-07-10 15:24:09 +0545

    user register using POST, also email same vayo vane status-status.HTTP_400_BAD_REQUEST

--- backend/base/views.py
+++ backend/base/views.py
@@ -16,6 +16,10 @@ from .serializers import ProductSerializer, UserSerializer, UserSerializerWithTo
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
@@ -28,13 +32,24 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         return data
 
 
-
-
-        return data
-
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user= User.objects.create(
+            first_name= data['name'],
+            username= data['email'],
+            email= data['email'],
+            password= make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status= status.HTTP_400_BAD_REQUEST)
 
 
 
